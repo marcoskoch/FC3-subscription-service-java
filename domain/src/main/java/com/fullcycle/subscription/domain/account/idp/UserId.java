4b265425ce0e3bd183a535c54eb3fbf5c660b074
Marcos Koch <marcos_koch@hotmail.com>
2026-06-09 14:23:59 -0300

    6.01-criando-a-entidade-user

--- domain/src/main/java/com/fullcycle/subscription/domain/account/idp/UserId.java
+++ domain/src/main/java/com/fullcycle/subscription/domain/account/idp/UserId.java
@@ -1,9 +1,14 @@
-package com.fullcycle.subscription.domain.account.iam;
+package com.fullcycle.subscription.domain.account.idp;
 
 import com.fullcycle.subscription.domain.Identifier;
 
 public record UserId(String value) implements Identifier<String> {
+
     public UserId {
         this.assertArgumentNotEmpty(value, "'userId' should not be empty");
+    }
+
+    public static UserId empty() {
+        return new UserId("");
     }
 }
